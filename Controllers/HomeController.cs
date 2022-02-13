0dfd273ccf6a2f179252f7859c4cea609b06439f
darkreplive <darkreplive@gmail.com>
2022-02-13 17:52:55 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            ViewBag.number = int.Parse(Request.Form["number"]);
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab3.Models
+{
+    public class Person
+    {
+        private string fName { get; set; }
+        private string lName { get; set; }
+        private int age { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+        private string desc { get; set; }
+    }
+}
